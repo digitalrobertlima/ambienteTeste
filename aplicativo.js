eeb629dc0816aaf1bd33d095078b6519c99992af
scr1pt3r <ge.crio123@gmail.com>
2022-04-22 14:04:21 -0300

    a m**** sendo feita

---
+++ aplicativo.js
@@ -0,0 +1,21 @@
+//APLICATIVO DE CLIENTE
+
+let telefone = 31982313684;
+let CEP = 30570000;
+
+if(telefone == 31982313684){
+	let nome = "Robert";
+	let endereco = "Rua Marcos Celho Neto, 227";
+	console.log(nome + " " + endereco);
+}
+
+/*
+
+function main(){
+	var hello = "OLÁ";
+	console.log(hello);
+}
+
+setInterval(main, 1000);
+
+*/
---
+++ app.js
@@ -0,0 +1,12 @@
+var x = 11;
+var y = 8;
+
+function soma(x, y) {
+	
+	var conta = x+y;
+	var mensagem = "Resposta: ";
+
+	console.log(mensagem + conta);
+}
+
+soma(x, y);
